ee4a41c3d51b229fb960227427d2f68db5bfd4d2
Jay <mjzeel@gmail.com>
2018-05-06 13:32:03 +0530

    OnsenUi Module added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { OnsenModule } from 'ngx-onsenui';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [
+		AppComponent
+	],
+	imports: [
+		BrowserModule,
+		OnsenModule
+	],
+	providers: [
+
+	],
+	schemas: [
+		CUSTOM_ELEMENTS_SCHEMA
+	],
+	bootstrap: [
+		AppComponent
+	]
 })
 export class AppModule { }
